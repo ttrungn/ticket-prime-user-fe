dbbe14ad8f27070352ec8e6663cec91e2179df60
ttrungn <trungnguyen0803forwork@gmail.com>
2025-04-02 15:54:03 +0700

    fix boostrap_s3.tf

--- terraform/dev/provision/bootstrap_s3.tf
+++ terraform/dev/provision/bootstrap_s3.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "state_bucket_lifecycle_config"
 }
 
 resource "aws_s3_bucket_policy" "deny_state_file_deletion" {
-  bucket = data.state_bucket.id
+  bucket = data.aws_s3_bucket.state_bucket.bucket
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_policy" "deny_state_file_deletion" {
         Effect    = "Deny"
         Principal = "*"
         Action    = "s3:DeleteObject"
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.state_bucket.bucket}/${var.bucket_name}/terraform.tfstate"
+        Resource  = "arn:aws:s3:::${data.aws_s3_bucket.state_bucket.bucket}/${var.bucket_name}/terraform.tfstate"
       }
     ]
   })
